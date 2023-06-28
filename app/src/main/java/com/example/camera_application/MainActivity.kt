edecc0d295230ec0835095a417bb180249feebdb
mohit6600 <mohitsingh2489@gmail.com>
2023-06-28 11:05:10 +0530

    add function capture image

--- app/src/main/java/com/example/camera_application/MainActivity.kt
+++ app/src/main/java/com/example/camera_application/MainActivity.kt
@@ -1,14 +1,20 @@
-
 package com.example.camera_application
+
 
 import android.content.pm.PackageManager
 import android.Manifest
+import android.content.ContentValues.TAG
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
@@ -16,7 +22,11 @@ import androidx.core.content.ContextCompat
 import androidx.camera.view.PreviewView
 import java.io.File
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,6 +49,52 @@ class MainActivity : AppCompatActivity() {
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSION, REQUEST_CODE_PERMISSION)
         }
+
+        findViewById<Button>(R.id.captureButton).setOnClickListener{
+            takePhoto()
+        }
+
+        outputDirectory=getOutputDirectory()
+        cameraExecutor=Executors.newSingleThreadExecutor()
+
+
+
+    }
+
+    private fun takePhoto() {
+
+        val imageCapture = imageCapture ?: return
+
+        val photoFile=File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT,Locale.US).format(System.currentTimeMillis())+".jpg"
+        )
+
+        val outputOption=ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOption,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback{
+
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG,"photo capture failed : ${exc.message}",exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults){
+                    val savedUri = Uri.fromFile(photoFile)
+
+                    findViewById<ImageView>(R.id.capture).visibility = View.VISIBLE
+                    findViewById<ImageView>(R.id.capture).setImageURI(savedUri)
+
+                    val msg = "photo capture succeded : $savedUri"
+                Toast.makeText(baseContext,msg,Toast.LENGTH_LONG).show()
+                    Log.d(TAG,msg)
+                }
+
+            }
+        )
+
     }
 
     private fun startCamera() {
@@ -68,6 +124,15 @@ class MainActivity : AppCompatActivity() {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
+    private fun getOutputDirectory():File{
+
+        val mediaDir=externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdir() }
+        }
+        return if(mediaDir!= null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -78,7 +143,8 @@ class MainActivity : AppCompatActivity() {
             if (allPermissionsGranted()) {
                 startCamera()
             } else {
-                Toast.makeText(this, "Permission not granted by the user", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Permission not granted by the user", Toast.LENGTH_SHORT)
+                    .show()
                 finish()
             }
         }
@@ -87,6 +153,13 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val Tag = "camera"
         private const val REQUEST_CODE_PERMISSION = 20
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    cameraExecutor.shutdown()
+    }
+
 }
